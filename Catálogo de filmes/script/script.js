6bd6394d25a56701883632cf662ed7c187a61213
kauavianaoliveira-rgb <kaua.viana.oliveira@escola.pr.gov.br>
2026-06-01 11:28:14 -0300

    Add files via upload

---
+++ Catálogo de filmes/script/script.js
@@ -0,0 +1,45 @@
+const filmes =[
+    {
+        titulo: "taxi drive",
+        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/hvJvOYRrF9xxvNzg2Jd9PYWu7c3.jpg",
+        descrição: "drive",
+    },
+nghfrfh      {
+        titulo: "diverti-damente",
+        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/4Kg34KTlnoHvjLzD7ji2jUSouvD.jpg",
+        descrição: "mente",
+    },
+      {
+        titulo: "psicopata americano",
+        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/rxT7rVM4oLODCzdkkMPWGPMTSt3.jpg",
+        descrição: "americano",
+    },
+     {
+        titulo: "Lucas neto em: acapamento de ferias 2",
+        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/vYgZa09ICwAB2TkonMfXRT4Dj8E.jpg",
+        descrição: "acapamento",
+    },
+     {
+        titulo: "miudos e graudos",
+        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/dbU5QOnTCPo4BKZskarx1bBG9lG.jpg",
+        descrição: "grados",
+    },
+     {
+        titulo: "kung fu futebol clube",
+        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/rO1YoR49hs9QPla0vwCxrMLln33.jpg",
+        descrição: "kung",
+    },
+
+]
+
+const listarfilmes = document getElementByid("listarfilmes")
+function mostrafilme(Lista){
+    listarfilmes.innerHTML = "
+    lista.array.forEach(filme => {
+        listarfilmes.innerHTML +=
+        <div class="col-md-4">
+            <div class="card card-filme h-100"
+
+
+    }
+
