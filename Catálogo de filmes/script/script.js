1e02bd7f3672c8bfd9aace559eb3b511e0eb7f89
kauavianaoliveira-rgb <kaua.viana.oliveira@escola.pr.gov.br>
2026-06-02 09:36:24 -0300

    catalogo de filmes

--- Catálogo de filmes/script/script.js
+++ Catálogo de filmes/script/script.js
@@ -1,45 +1,52 @@
-const filmes =[
+const filmes = [
     {
-        titulo: "taxi drive",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/hvJvOYRrF9xxvNzg2Jd9PYWu7c3.jpg",
-        descrição: "drive",
+        titulo: "Vingadores",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/9fRX8UKlIW7Lb9GqNsJVakWWFCi.jpg",
+        descricao: "Ultimato",
     },
-nghfrfh      {
-        titulo: "diverti-damente",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/4Kg34KTlnoHvjLzD7ji2jUSouvD.jpg",
-        descrição: "mente",
+    {
+        titulo: "Batman",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/4lj1ikfsSmMZNyfdi8R8Tv5tsgb.jpg",
+        descricao: "Cavaleiro das trevas",
     },
-      {
-        titulo: "psicopata americano",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/rxT7rVM4oLODCzdkkMPWGPMTSt3.jpg",
-        descrição: "americano",
+    {
+        titulo: "Homem-Aranha",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/xaKydnMw6wR1MBAjS5seGPVusbs.jpg",
+        descricao: "Sem volta para casa",
     },
-     {
-        titulo: "Lucas neto em: acapamento de ferias 2",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/vYgZa09ICwAB2TkonMfXRT4Dj8E.jpg",
-        descrição: "acapamento",
+    {
+        titulo: "As Branquelas",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/aJZOcorpgloDLkPP6ED0t9sXjNu.jpg",
+        descricao: "Dois agentes do FBI atrapalhados",
     },
-     {
-        titulo: "miudos e graudos",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/dbU5QOnTCPo4BKZskarx1bBG9lG.jpg",
-        descrição: "grados",
+    {
+        titulo: "Projeto Almanaque",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/2krLUKMsKbnZk6j647IlkqlMZ2u.jpg",
+        descricao: "Jovens descobem uma máquina do tempo",
     },
-     {
-        titulo: "kung fu futebol clube",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/rO1YoR49hs9QPla0vwCxrMLln33.jpg",
-        descrição: "kung",
-    },
-
+    {
+        titulo: "Mortal Kombat",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/4nW5X9iBrMlHMDcTsEOQWXKu3TZ.jpg",
+        descricao: "Lutando contra inimigos da Exoterra",
+    }
 ]
 
-const listarfilmes = document getElementByid("listarfilmes")
-function mostrafilme(Lista){
-    listarfilmes.innerHTML = "
-    lista.array.forEach(filme => {
-        listarfilmes.innerHTML +=
-        <div class="col-md-4">
-            <div class="card card-filme h-100"
-
-
-    }
+const listaFilmes = document.getElementById("listaFilmes");
+function mostrarFilmes(lista) {
+    listaFilmes.innerHTML = ""
+    lista.forEach(filme => {
+        listaFilmes.innerHTML += `
+            <div class="col-md-4">
+                <div class="card card-filme h-100">
+                    <img src="${filme.imagem}" class="card-img-top">
+                    <div class="card-body">
+                        <h3>${filme.titulo}</h3>
+                        <p>${filme.descricao}</p>
+                        <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes"</button>
+                    </div>
+                </div>
+            </div>    
+        `
+    })
+}
 
